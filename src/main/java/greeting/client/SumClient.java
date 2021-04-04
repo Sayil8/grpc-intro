737f8ee4bede3a4b34cb83c3e1e8bf88f0e6639f
smuehr <sayil.muehr@trivago.com>
2021-04-04 19:32:53 +0200

    Sum client refactor

--- src/main/java/greeting/client/SumClient.java
+++ src/main/java/greeting/client/SumClient.java
@@ -1,20 +1,49 @@
 package greeting.client;
 
-import com.proto.sum.Sum;
-import com.proto.sum.SumRequest;
-import com.proto.sum.SumResponse;
-import com.proto.sum.SumServiceGrpc;
+import com.proto.sum.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class SumClient {
+
     public static void main(String[] args) {
         System.out.println("Hello I am a grpc client");
+        SumClient client = new SumClient();
 
+        client.run();
+
+    }
+
+    private void run() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
+        doUnaryCall(channel);
+        doServerStreamingCall(channel);
+
+
+        System.out.println("Shuting down channel");
+        channel.shutdown();
+    }
+
+    private void doServerStreamingCall(ManagedChannel channel) {
+        System.out.println("Creating stub");
+
+        // create a service client
+        SumServiceGrpc.SumServiceBlockingStub stub = SumServiceGrpc.newBlockingStub(channel);
+
+        // streaming server
+        int number = 567890;
+        stub.primeDecomposition(PrimeDecompositionRequest.newBuilder()
+                .setNumber(number)
+                .build())
+                .forEachRemaining(primeDecompositionResponse -> {
+                    System.out.println(primeDecompositionResponse.getResult());
+                });
+    }
+
+    private void doUnaryCall(ManagedChannel channel) {
         System.out.println("Creating stub");
 
         // create a service client
@@ -32,11 +61,6 @@ public class SumClient {
         // call the RPC and get back a GreetResponse
         SumResponse sumResponse = sumClient.sum(sumRequest);
 
-
         System.out.println(sumResponse.getResult());
-
-
-        System.out.println("Shuting down channel");
-        channel.shutdown();
     }
 }
