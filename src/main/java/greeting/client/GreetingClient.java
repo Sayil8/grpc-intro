939809e7d95a81858eddc55a5e52965d67a35ed1
smuehr <sayil.muehr@trivago.com>
2021-04-04 18:40:22 +0200

    Greeting client refactor

--- src/main/java/greeting/client/GreetingClient.java
+++ src/main/java/greeting/client/GreetingClient.java
@@ -1,21 +1,32 @@
 package greeting.client;
 
-import com.proto.greet.GreetRequest;
-import com.proto.greet.GreetResponse;
-import com.proto.greet.GreetServiceGrpc;
-import com.proto.greet.Greeting;
+import com.proto.greet.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class GreetingClient {
 
     public static void main(String[] args) {
+
         System.out.println("Hello I am a grpc client");
 
+        GreetingClient main = new GreetingClient();
+        main.run();
+    }
+
+    private void run() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
+        doUnaryCall(channel);
+        doServerStreamingCall(channel);
+
+        System.out.println("Shuting down channel");
+        channel.shutdown();
+    }
+
+    private void doUnaryCall(ManagedChannel channel) {
         System.out.println("Creating stub");
 
         // create a service client
@@ -35,8 +46,25 @@ public class GreetingClient {
 
         System.out.println(greetResponse.getResult());
 
+    }
 
-        System.out.println("Shuting down channel");
-        channel.shutdown();
+    private void doServerStreamingCall(ManagedChannel channel) {
+        System.out.println("Creating stub");
+        // create a service client
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        // server streaming
+        //we prepare the request
+        GreetManyTimesRequest greetManyTimesRequest = GreetManyTimesRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder().setFirstName("Sayil"))
+                .build();
+
+        // we stream the response in a blocking manner
+        greetClient.greetManyTimes(greetManyTimesRequest)
+                .forEachRemaining(greetManyTimesResponse -> {
+                    System.out.println(greetManyTimesResponse.getResult());
+                });
+
+
     }
 }
